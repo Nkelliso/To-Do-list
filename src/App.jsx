5d90dbf1b537aa611cdce7c834ea4e55e291eff0
Nkelliso <nathankellisonmiller@gmail.com>
2026-04-05 11:44:12 -0400

    add version label

--- src/App.jsx
+++ src/App.jsx
@@ -67,6 +67,10 @@ export default function App() {
         </div>
       </div>
 
+      <div className="fixed bottom-2 right-3 text-[10px] text-gray-700 opacity-40 pointer-events-none select-none z-50">
+        v1.1
+      </div>
+
       {showModal && (
         <AddTaskModal
           onClose={() => setShowModal(false)}
